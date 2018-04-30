25b873e0857f639322b782a7f9fe70271621d5a1
Rob Bertels <bertels.rob@gmail.com>
2018-04-30 19:55:21 +0200

    Giraffe Host

---
+++ FsWebTests/GiraffeHost/Program.fs
@@ -0,0 +1,25 @@
+﻿open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Hosting
+open System
+
+let webApp = route "/" >=> text "hello world!"
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main argv = 
+    
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+
+    0
